fe06458478f92d2a89963805e66e08cb29bb8373
diogoalmeidacunha <diogocunha588@gmail.com>
2018-10-29 17:35:18 +0000

    circular animation done

--- CircularAnimation.js
+++ CircularAnimation.js
@@ -1,5 +1,7 @@
 var DEGREE_TO_RAD = (Math.PI / 180);
 
+
+
 /**
  * CircularAnimation
  * @constructor
@@ -9,19 +11,47 @@ class CircularAnimation extends Animation
 	constructor(scene, id, durationTime, centerCoords, radius, initialAngle, rotationAngle) 
 	{
         super(scene, id, durationTime);
-        this.centerCoords = centerCoords;
+
+        //Pontos do Centro
+        this.centerX = centerCoords[0];
+        this.centerY = centerCoords[1];
+        this.centerZ = centerCoords[2];
+
+        //Angles
         this.radius = radius;
-        this.initialAngle = initialAngle;
-        this.rotationAngle = rotationAngle;
+        this.initialAngle = initialAngle * DEGREE_TO_RAD;
+        this.rotationAngle = rotationAngle * DEGREE_TO_RAD;
+
+        //rotAnd = rotacao = 20º   and Radius = raio = 5
+        this.totalDistance = this.rotationAngle * this.radius;
+
+        this.angularVelocity = (this.totalDistance/this.time);
+
+        this.atualdist = 0;
+        this.atualAngle = 0;
+        this.animationReachedLoop = false;
+    };
+   
+    update(deltaTime){
+        // Velocidade angular
+        if(this.atualAngle < this.rotationAngle){
+            this.atualdist += this.angularVelocity*deltaTime;
+            this.atualAngle = this.atualdist/this.radius;
+        } else{
+            this.atualdist = 0;
+            this.atualAngle = 0;
+            this.animationReachedLoop = true;
+        }
+
     };
 
     apply(){
-        //console.log("here as well but circular");
-    };
-
-    
-    update(deltaTime){
-        //console.log("doing nothing circular");
+        //Por objto na origem
+        this.scene.translate(this.centerX, this.centerY, this.centerZ);
+        //Movimento de Translacao
+        this.scene.translate(this.radius * Math.cos(this.initialAngle + this.atualAngle), 0, -this.radius  * Math.sin(this.initialAngle + this.atualAngle));
+        //Movimento de rotacao
+        this.scene.rotate(Math.PI + this.initialAngle + this.atualAngle, 0, 1, 0);
     };
 
 };
