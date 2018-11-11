c44a2ed8a1597dd1d5a3aa91f1296c725cdceb87
Unknown <luis.alvela@gmail.com>
2018-11-11 16:23:28 +0000

    MyPlane class (point number 1) completed

--- primitives/MyPlane.js
+++ primitives/MyPlane.js
@@ -20,6 +20,24 @@ class MyPlane extends CGFobject
 		var degree2 = 1;
 		var controlvertexes = [	[ [-0.5, 0.0, 0.5, 1], [-0.5,  0.0, -0.5, 1] ], [ [0.5, 0.0, 0.5, 1], [0.5,  0.0, -0.5, 1] ] ];
 		var nurbsSurface = new CGFnurbsSurface(degree1, degree2, controlvertexes);
-		var obj = new CGFnurbsObject(this.scene, this.npartsU, this.npartsV, nurbsSurface); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
+		this.nurbs = new CGFnurbsObject(this.scene, this.npartsU, this.npartsV, nurbsSurface); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
+	};
+
+	display()
+	{
+		this.nurbs.display();
+	};
+
+
+	// Don't know if we will need to apply textures to NURBS / if it is applied in the same way.
+
+	scaleTextureCoords(tex1, tex2)
+	{
+	
+	};
+
+	resetCoords()
+	{
+
 	};
 };
